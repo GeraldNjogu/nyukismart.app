e0938c6db643132b6cd626bfbe65f1ac89abf511
GeraldNjogu <147712617+GeraldNjogu@users.noreply.github.com>
2024-07-30 22:13:20 +0300

    Create cover.js

---
+++ cover.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const helpBtn = document.getElementById('loginBtn');
+    loginBtn.addEventListener('click', () => {
+        window.location.href = 'login.html'; // Replace 'help.html' with the path to your help page
+    });
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const helpBtn = document.getElementById('signupBtn');
+    signupBtn.addEventListener('click', () => {
+        window.location.href = 'sign up.html'; // Replace 'help.html' with the path to your help page
+    });
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const helpBtn = document.getElementById('helpBtn');
+    helpBtn.addEventListener('click', () => {
+        window.location.href = 'help.html'; // Replace 'help.html' with the path to your help page
+    });
+});
+
